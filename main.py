402d18324df26370b73f8f8e53e9adc32833f600
ooooo5h <ooooo5h91@gmail.com>
2021-12-24 15:21:06 +0900

    사용자 클래스 추가 -> main에서 객체로 생성

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from book import Book
+from user import User
 
 book1 = Book('먼나라 이웃나라', 1500, 0)
+
+user1 = User('김성인', 1990, 20000)
---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, name, birth_year, point):
+        self.name = name
+        self.birth_year = birth_year
+        self.point = point
+        
