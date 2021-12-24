78a968500393b80958a58d645d9190800a6cbb5d
ooooo5h <ooooo5h91@gmail.com>
2021-12-24 17:14:42 +0900

    모듈 내부의 -> 특정 함수만 불러오기

--- my_main.py
+++ my_main.py
@@ -1,4 +1,5 @@
-# (my_math)모듈을 불러와서, 그 안의 함수(add) 사용
-import my_math
+# (my_math)모듈 내부의 함수(add)를 따로 불러와서 바로 사용
+# import my_math 로 활용하기 번거로웡!!
 
-my_math.add(10,20)
+from my_math import add
+add(10, 20)
