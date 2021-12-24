e7893c7778ea161078ca17b3b71b1c8c3a743ca6
ooooo5h <ooooo5h91@gmail.com>
2021-12-24 16:45:10 +0900

    카드 클래스의 가로/세로길이 추가
    - 모든 카드 객체가, 같은 값을 공유

--- main_card.py
+++ main_card.py
@@ -3,3 +3,7 @@ from card import Card
 # 두장의 카드 생성
 card1 = Card('스페이드', 2)
 card2 = Card('하트', 7)
+
+# 두 장의 카드 각각의 가로길이를 출력해보자
+print(f'스페이드2의 가로길이? : {card1.width}')
+print(f'하트7의 가로 길이 : {card2.width}')
