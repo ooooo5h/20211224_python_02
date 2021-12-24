b59c121600aa1cee52d1c1081c9f1a543c7a9f57
ooooo5h <ooooo5h91@gmail.com>
2021-12-24 17:23:21 +0900

    모든 함수 일괄 소환

--- my_main.py
+++ my_main.py
@@ -1,7 +1,7 @@
 # (my_math)모듈 내부의 함수(add)를 따로 불러와서 바로 사용
 # import my_math 로 활용하기 번거로웡!!
 
-from my_math import add, mul, div  # 하나의 모듈ㅇ에서 여러개의 함수를 한꺼번에 불러올 수 있다. 
+from my_math import *  # 해당 모듈의 모든 함수 불러내기
 
 mul(10, 20)
 add(10, 20)
