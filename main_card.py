78e70847e4f600c39c61e7dd0df54d3ef1459bb6
ooooo5h <ooooo5h91@gmail.com>
2021-12-24 16:51:30 +0900

    클래스 변수에 대한 체험

--- main_card.py
+++ main_card.py
@@ -4,6 +4,16 @@ from card import Card
 card1 = Card('스페이드', 2)
 card2 = Card('하트', 7)
 
+# card의 가로길이(Card 클래스 자체의 속성)만 4로 변경
+Card.width = 4
+# Card의 가로/세로길이는 클래스 변수라서 Card.변수로 접근이 가능하다.
+# 하지만, 각 카드 객체의 무늬/숫자는 각 객체에 연결된 변수라서 Card.변수로는 접근이 불가능
+# print(Card.pattern)    >> 이런 행위는 불가능!! 
+
 # 두 장의 카드 각각의 가로길이를 출력해보자
 print(f'스페이드2의 가로길이? : {card1.width}')
 print(f'하트7의 가로 길이 : {card2.width}')
+# 위처럼 잘 안쓰고
+
+# 아래처럼 쓴다. 클래스 자체의 변수
+print(f'카드 전체의 가로길이 : {Card.width}')
