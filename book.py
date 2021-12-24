7320c6f17c38a79c87c03fb2614ada1f9eac9258
ooooo5h <ooooo5h91@gmail.com>
2021-12-24 15:18:33 +0900

    메인 코드와 책 클래스 (생성자 생성)

---
+++ book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, rent_fee, limit_age):
+        self.title = title
+        self.rent_fee = rent_fee
+        self.limit_age = limit_age
+        
---
+++ main.py
@@ -0,0 +1,3 @@
+from book import Book
+
+book1 = Book('먼나라 이웃나라', 1500, 0)
