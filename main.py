05f59bf05a730410e5f8339f9cf0e80cc171ae31
ooooo5h <ooooo5h91@gmail.com>
2021-12-24 15:46:17 +0900

    사용자 메쏘드 2개 추가
    - 년도를 파라미터로 받아서, 나이를 리턴
    - 사용자 정보를 출력 => self가 가지고 있는 메쏘드를 활용하기( 다른 메쏘드 호출 가능 )

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ from user import User
 book1 = Book('먼나라 이웃나라', 1500, 0)
 
 user1 = User('김성인', 1990, 20000)
+
+
+user1.print_user_info()
--- user.py
+++ user.py
@@ -7,3 +7,9 @@ class User:
     def get_age(self, year):
         age = year - self.birth_year + 1
         return age
+
+    def print_user_info(self):
+        print('===== 사용자 정보 출력 =====')
+        print(f'이름 : {self.name}')
+        print(f'2021년의 나이 : {self.get_age(2021)}세')
+        print(f'포인트 : {self.point}P') 
